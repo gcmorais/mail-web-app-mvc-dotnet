77bf5933e407667cd36b09afbc06ccb573a95b0b
gcmorais <guilhermeclarodemorais@gmail.com>
2024-08-22 12:53:24 -0300

    refactor(email): creating model and generating migrations

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using mail_web_app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mail_web_app.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<EmailModel> Emails { get; set; }
+    }
+}
---
+++ src/Models/EmailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace mail_web_app.Models
+{
+    public class EmailModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public DateTime DataDeRegistro { get; set; } = DateTime.Now;
+    }
+}
