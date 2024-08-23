8ec19aa6e438ac2e68e47bd7a91ed164eafb405f
gcmorais <guilhermeclarodemorais@gmail.com>
2024-08-23 10:44:25 -0300

    style(home): making the page responsive

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$("#email_form").submit(function (e) {
+    e.preventDefault();
+});
