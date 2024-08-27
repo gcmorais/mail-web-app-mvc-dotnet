189ab7e9565d9c473e6bf6e4870c1d48e96492a6
gcmorais <guilhermeclarodemorais@gmail.com>
2024-08-27 11:52:02 -0300

    refactor(Email Model): add required models

--- src/Models/EmailModel.cs
+++ src/Models/EmailModel.cs
@@ -6,7 +6,9 @@ namespace mail_web_app.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter Name!")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Enter Email!")]
         public string Email { get; set; }
         public bool Status { get; set; } = true;
         public DateTime DataDeRegistro { get; set; } = DateTime.Now;
